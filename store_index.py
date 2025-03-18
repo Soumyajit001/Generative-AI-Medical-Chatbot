5104d8a75204a0781f7d61b7999ded599540b6d0
Soumyajit001 <soumyajitpal416@gmail.com>
2025-03-18 15:31:25 +0530

    chatbot template added

--- store_index.py
+++ store_index.py
@@ -5,18 +5,22 @@ from langchain_pinecone import PineconeVectorStore
 from dotenv import load_dotenv
 import os
 
+
 load_dotenv()
 
 PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
 os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
 
+
 extracted_data = load_pdf_file(data='D:/AI - Agents/ai_medical_agent/Generative-AI-Medical-Chatbot/Book/')
 text_chunks = text_split(extracted_data)
 embeddings = download_hugging_face_embeddings()
 
+
 pc = Pinecone(api_key=PINECONE_API_KEY)
 
 index_name = "medical-bot"
+
 
 pc.create_index(
     name=index_name,
@@ -29,9 +33,6 @@ pc.create_index(
 )
 
 # Embed each chunk and upsert the embeddings into your Pinecone index.
-
-from langchain_pinecone import PineconeVectorStore
-
 docsearch = PineconeVectorStore.from_documents(
     documents = text_chunks,
     index_name = index_name,
